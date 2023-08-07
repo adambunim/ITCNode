2d9158e8d65380fa77705203a005e6995ec4c2ac
adambunim <adambunim@gmail.com>
2023-08-07 12:37:17 +0300

    delete

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,22 @@ import * as fs from 'fs';
 
 const app = express();
 const port = 3000;
+const fileName = 'items.json';
 
 app.get('/', (req, res) => {
   console.log('get');
-  const text = fs.readFileSync('items.json','utf8');
+  const text = fs.readFileSync(fileName,'utf8');
   res.send(text);
 });
 
 app.delete('/:id', (req, res) => {
-  console.log('delete');
+  let id = req.params.id
+  console.log('delete ' + id);
+  let text = fs.readFileSync(fileName,'utf8');
+  let items = JSON.parse(text);
+  let filtered = items.filter(item => item.id !== id);
+  let firteredText = JSON.stringify(filtered, null, 2);
+  fs.writeFileSync(fileName, firteredText);
   res.send("ok");
 });
 
